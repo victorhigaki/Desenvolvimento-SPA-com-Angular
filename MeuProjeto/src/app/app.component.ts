5395c0c30c18613a45305732c41859b3a8acf296
Victor Higaki <victorhigaki@outlook.com>
2024-09-06 06:52:44 -0300

    Dando uma cara a aplicação

--- MeuProjeto/src/app/app.component.ts
+++ MeuProjeto/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet, RouterModule],
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
