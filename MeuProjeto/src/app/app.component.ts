62eff9e69ef2c87490ef6bec1caa50757af667e2
Victor Higaki <victorhigaki@outlook.com>
2024-09-06 13:32:29 -0300

    Definindo os componentes de navegação

--- MeuProjeto/src/app/app.component.ts
+++ MeuProjeto/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, MenuComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
